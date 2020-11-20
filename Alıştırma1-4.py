1884927fdc9e4b046873c1a7b1880c7f52766244
dogaberkayerdek <57464323+dogaberkayerdek@users.noreply.github.com>
2020-11-20 05:31:07 +0300

    Delete Alıştırma1-4.py

--- Alıştırma1-4.py
+++
@@ -1,9 +0,0 @@
-def grupla(liste):
-    liste1=[[],[]]
-    for i in range (len(liste)):
-        if liste[i] % 2 == 0:
-            liste1[1].append(liste[i])
-        else:
-            liste1[0].append(liste[i])
-    print(liste1)
-
