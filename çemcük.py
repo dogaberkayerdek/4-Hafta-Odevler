ed48102357e06f5f2bb167ee49a21b0de6dc5890
dogaberkayerdek <57464323+dogaberkayerdek@users.noreply.github.com>
2020-11-20 05:26:54 +0300

    Add files via upload

---
+++ çemcük.py
@@ -0,0 +1,14 @@
+
+liste=[]
+for i in range (0,5):
+    a=input("İsim :")
+    b=input("Soy isim:")
+    c=input("Takımı:")
+    if c == 'Barcelona':
+        liste.append([a,b,c])
+print(liste)
+            
+    
+
+            
+    
